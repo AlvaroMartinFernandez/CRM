2db2faa9a3c4a556e5742168c82d3d44b387821b
AlvaroMartinFernandez <alvaro.martin@edu.uah.es>
2023-05-06 16:40:34 -0500

    Inicio Fronted

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-
-function App() {
+import React from 'react';
+import Header from './componets/header/Header';
+import Sidebar from './componets/sidebar/Sidebar';
+import Administradores from './componets/administradores/Administradores';
+export default function App() {
   return (
-    <div className="App">
-hola mundo
+    <div className="sidebar-mini">
+      <div className="wrapper">
+        <Header/>
+        <Sidebar/>
+        <Administradores/>
+      </div>
     </div>
   );
 }
-
-export default App;
